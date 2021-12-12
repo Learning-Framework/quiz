8e60c2c70e9eaaea617b6ab3d5438ebbeaba3738
dmitriy.kravets <Dmitriy.Kravets@wolterskluwer.com>
2021-12-13 01:05:02 +0300

    [gophersies] quiz: correct and incorrect answers printed at the end.

--- students/dkravets/main.go
+++ students/dkravets/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -10,19 +11,32 @@ import (
 
 func main() {
 	fname := *flag.String("path", "problems.csv", "path to a csv file with quiz questions")
+	flag.Parse()
+
 	fl, err := os.Open(fname)
 	if err != nil {
 		log.Fatalln("Open quiz csv file: ", err)
 	}
-
 	csvr := csv.NewReader(fl)
-
-	record, err := csvr.ReadAll()
+	records, err := csvr.ReadAll()
 	if err != nil {
 		log.Fatalln("error reading csv : ", err)
 	}
-	for _, line := range record {
-		fmt.Printf("Question: %s,\t answer: %s\n", line[0], line[1])
+
+	var correct, incorrect int
+	for _, line := range records {
+		fmt.Printf("Question: %s:\t", line[0])
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		if scanner.Text() != line[1] {
+			fmt.Println("Incorrect! Correct answer is : ", line[1])
+			incorrect += 1
+		} else {
+			correct += 1
+			continue
+		}
 	}
+
+	fmt.Printf("Correct answers: %d\t incorrect answers: %d\n", correct, incorrect)
 
 }
